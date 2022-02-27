bdf80550801e151ecc3b057adf21aed945b48971
Vadim Kirpun <quinze105@gmail.com>
2022-02-27 16:41:18 +0300

    add JSDoc to formatCurrency

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,3 +1,18 @@
+/**
+ * @function formatCurrency
+ * Format number as currency (US Dollars)
+ *
+ * @param {number} amount
+ * @returns {string} formatted number
+ *
+ * @example
+ *  formatCurrency(0)
+ *  // => $0.00
+ *
+ * @example
+ *  formatCurrency(1.5)
+ *  // => $1.50
+ */
 export const formatCurrency = (amount) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
